cf6533a40a9da142b099c7b7355bc2b452e344d2
MickNabewata <aquarius.mikito.0123@gmail.com>
2020-11-24 14:12:15 +0900

    update

--- src/webparts/spfxKqlExplorer/components/iconButton/IconButton.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { IconButton as OriginalIconButton, IButtonProps } from 'office-ui-fabric-react';
-import styles from './IconButton.module.scss';
-
-/** スタイル付きボタン */
-export default function IconButton(props: IButtonProps) {
-    return <OriginalIconButton {...props} className={`${styles.iconButton}${props.className ? ` ${props.className}` : ''}`} />;
-}
